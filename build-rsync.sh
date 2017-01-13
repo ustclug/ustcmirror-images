c1b4b791fabde520ebdb4c30c84193a9138a1982
knight42 <anonymousknight96@gmail.com>
2017-01-13 13:50:03 +0800

    Add build-rsync

---
+++ build-rsync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+f="$MIRROR/Dockerfile"
+
+cat << EOF > "$f"
+FROM ustcmirror/base:alpine
+MAINTAINER Jian Zeng <anonymousknight96 AT gmail.com>
+ENTRYPOINT ["/sync.sh"]
+ADD sync.sh /
+RUN apk update && apk add --update rsync && \
+    rm -rf /var/cache/apk/*
+EOF
+
+docker build -t "$ORG/$MIRROR" "$MIRROR"
+docker push "$ORG/$MIRROR"
+
+rm "$f"
