7c009324727c7111b337ae87b26ebd10745dd374
knight42 <anonymousknight96@gmail.com>
2017-01-13 13:49:10 +0800

    Add build-base

---
+++ build-base.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+tag='ustcmirror/base:debian'
+
+cat << EOF > "base/Dockerfile"
+FROM debian:jessie-slim
+MAINTAINER Jian Zeng <anonymousknight96 AT gmail.com>
+ENTRYPOINT ["/entry.sh"]
+ADD entry.sh /
+EOF
+
+docker build -t "$tag" base
+docker push "$tag"
+
+########################################
+
+tag='ustcmirror/base:alpine'
+
+cat << EOF > "base/Dockerfile"
+FROM alpine:3.5
+MAINTAINER Jian Zeng <anonymousknight96 AT gmail.com>
+ENTRYPOINT ["/entry.sh"]
+ADD entry.sh /
+RUN apk update && apk add --update bash tzdata && rm -rf /var/cache/apk/* \
+    && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+EOF
+
+docker build -t "$tag" base
+docker push "$tag"
