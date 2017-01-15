3d2bb67c551aea7131918cc61c9f414c254f6b4d
knight42 <anonymousknight96@gmail.com>
2017-01-15 10:20:06 +0800

    Update rsync

--- rsync/sync.sh
+++ rsync/sync.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-sleep 6
+## SET IN ENVIRONMENT VARIABLES
+#BIND_ADDRESS=
+#TO=
 
-env
+#RSYNC_USER=
+#RSYNC_HOST=
+#RSYNC_PASSWORD=
 
-exit 0
+#RSYNC_PATH=
+
+#RSYNC_BW=
+#RSYNC_OPTIONS=
+#RSYNC_EXCLUDE=
+#RSYNC_MAXDELETE=
+
+set -e
+[[ $DEBUG = true  ]] && set -x
+
+[[ -n $RSYNC_USER ]] && RSYNC_HOST="$RSYNC_USER@$RSYNC_HOST"
+
+RSYNC_BW=${RSYNC_BW:-0}
+RSYNC_MAXDELETE=${RSYNC_MAXDELETE:-4000}
+RSYNC_OPTIONS=${RSYNC_OPTIONS:-'-4pPrltvHSB8192 --partial-dir=.rsync-partial --timeout 14400 --delay-updates --safe-links --delete-delay --delete-excluded'}
+RSYNC_EXCLUDE+=' --exclude .~tmp~/'
+
+rsync $EXCLUDE --bwlimit "$RSYNC_BW" --max-delete "$RSYNC_MAXDELETE" --address "$BIND_ADDRESS" $RSYNC_OPTIONS "${RSYNC_HOST}::${RSYNC_PATH}" "$TO"
