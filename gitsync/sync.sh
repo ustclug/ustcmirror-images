d27d1a3ed42997a44bd5e782314a8382c8051c20
knight42 <anonymousknight96@gmail.com>
2017-01-15 10:20:21 +0800

    Update gitsync

--- gitsync/sync.sh
+++ gitsync/sync.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-sleep 6
+## SET IN ENVIRONMENT VARIABLES
+#TO=
 
-env
+#GITSYNC_URL=
+#GITSYNC_BRANCH=
+#GITSYNC_REMOTE=
+#GITSYNC_BITMAP=
 
-exit 0
+set -e
+[[ $DEBUG = true ]] && set -x
+
+GITSYNC_REMOTE="${GITSYNC_REMOTE:-origin}"
+GITSYNC_BRANCH="${GITSYNC_BRANCH:-master:master}"
+GITSYNC_BITMAP="${GITSYNC_BITMAP:-false}"
+
+[[ ! -d $GITSYNC_TO ]] && git clone --bare "$GITSYNC_URL" "$TO"
+
+cd "$GITSYNC_TO" || exit 1
+git fetch "$GITSYNC_REMOTE" "$GITSYNC_BRANCH" -v --progress
+git update-server-info
+
+if [[ -z $DEBUG || $DEBUG = false ]]; then
+    git gc --auto
+    [[ $GITSYNC_BITMAP = true ]] && git repack -abd
+fi
