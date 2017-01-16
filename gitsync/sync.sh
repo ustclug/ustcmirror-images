35b9547ad919719bb48b0eaa22231d014283f409
knight42 <anonymousknight96@gmail.com>
2017-01-16 18:56:20 +0800

    gitsync: update sync.sh

--- gitsync/sync.sh
+++ gitsync/sync.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-## SET IN ENVIRONMENT VARIABLES
+## EXPORTED IN entry.sh
 #TO=
+#LOGDIR=
 
+## SET IN ENVIRONMENT VARIABLES
 #GITSYNC_URL=
 #GITSYNC_BRANCH=
 #GITSYNC_REMOTE=
 #GITSYNC_BITMAP=
+
+is_empty() {
+    [[ -z $(ls -A "$1" 2>/dev/null) ]]
+}
 
 set -e
 [[ $DEBUG = true ]] && set -x
@@ -15,9 +21,9 @@ GITSYNC_REMOTE="${GITSYNC_REMOTE:-origin}"
 GITSYNC_BRANCH="${GITSYNC_BRANCH:-master:master}"
 GITSYNC_BITMAP="${GITSYNC_BITMAP:-false}"
 
-[[ ! -d $TO ]] && git clone --bare "$GITSYNC_URL" "$TO"
+is_empty "$TO" && git clone -v --progress --bare "$GITSYNC_URL" "$TO"
 
-cd "$GITSYNC_TO" || exit 1
+cd "$TO" || exit 1
 git fetch "$GITSYNC_REMOTE" "$GITSYNC_BRANCH" -v --progress
 git update-server-info
 
