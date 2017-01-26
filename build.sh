d18e2b13def0a2cc68b4077bb444d4708587df66
knight42 <anonymousknight96@gmail.com>
2017-01-26 21:42:50 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 set -e
-[[ $DEBUG = [tT]rue ]] && set -x
-LAST="${LAST:-HEAD^}"
+[[ $DEBUG = true ]] && set -x
 export ORG=ustcmirror
 
 is_modified() {
-    [[ -n $(git diff "$LAST" HEAD -- "$1") ]]
+    [[ -z $TRAVIS_COMMIT_RANGE ]] && return 1 # false
+    local COMMIT_FROM=${TRAVIS_COMMIT_RANGE%...*}
+    local COMMIT_TO=${TRAVIS_COMMIT_RANGE#*...}
+    [[ -n $(git diff "$COMMIT_FROM" "$COMMIT_TO" -- "$1") ]]
 }
 
 build_image() {
