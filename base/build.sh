c97d3f3b6112bdbb088e9e8171c3badab7c5cc5c
Yifan Gao <git@gaoyifan.com>
2017-02-03 12:38:09 +0800

    refactor base image

--- base/build.sh
+++ base/build.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
 set -e
-tag='ustcmirror/base:debian'
+set -x
 
-cat << EOF > Dockerfile
-FROM debian:jessie-slim
-MAINTAINER Jian Zeng <anonymousknight96 AT gmail.com>
-RUN echo 'Asia/Shanghai' > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
-ADD ["entry.sh", "savelog", "/usr/local/bin/"]
-EOF
-
-docker build -t "$tag" --label ustcmirror.images .
-docker push "$tag"
-
-########################################
-
-tag='ustcmirror/base:alpine'
-
-cat << EOF > Dockerfile
-FROM alpine:3.5
-MAINTAINER Jian Zeng <anonymousknight96 AT gmail.com>
-RUN apk update && apk add --update bash tzdata && rm -rf /var/cache/apk/* \
-    && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-ADD ["entry.sh", "savelog", "/usr/local/bin/"]
-EOF
-
-docker build -t "$tag" --label ustcmirror.images .
-docker push "$tag"
-
-rm Dockerfile
+ls Dockerfile.* | 
+while read DOCKERFILE; do
+    tag="ustcmirror/base:${DOCKERFILE##*.}"
+    docker build -f $DOCKERFILE -t "$tag" --label ustcmirror.images .
+    docker push "$tag"
+done
