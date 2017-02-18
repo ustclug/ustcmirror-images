2c32e6504680991fab287d914b12ff6d3547805a
knight42 <anonymousknight96@gmail.com>
2017-02-18 20:25:03 +0800

    rsync: update opts

--- rsync/sync.sh
+++ rsync/sync.sh
@@ -6,27 +6,43 @@
 #LOGFILE=
 
 ## SET IN ENVIRONMENT VARIABLES
+#PREFER_IPV6=
 #BIND_ADDRESS=
 
 #RSYNC_USER=
 #RSYNC_HOST=
 #RSYNC_PASSWORD=
-
 #RSYNC_PATH=
-
 #RSYNC_BW=
-#RSYNC_OPTIONS=
 #RSYNC_EXCLUDE=
 #RSYNC_MAXDELETE=
+#RSYNC_TIMEOUT=
+#RSYNC_BLKSIZE=
+#RSYNC_EXTRA_OPTS=
+#RSYNC_DELAY_UPDATES=
 
 set -e
 [[ $DEBUG = true ]] && set -x
 
+RSYNC_BW=${RSYNC_BW:-0}
+RSYNC_BLKSIZE="${RSYNC_BLKSIZE:-8192}"
+RSYNC_TIMEOUT="${RSYNC_TIMEOUT:-14400}"
+RSYNC_MAXDELETE=${RSYNC_MAXDELETE:-4000}
+RSYNC_DELAY_UPDATES="${RSYNC_DELAY_UPDATES:-true}"
+
+OPTS="-pPrltvHS --partial-dir=.rsync-partial --timeout ${RSYNC_TIMEOUT} --safe-links --delete-delay --delete-excluded"
+
 [[ -n $RSYNC_USER ]] && RSYNC_HOST="$RSYNC_USER@$RSYNC_HOST"
 
-RSYNC_BW=${RSYNC_BW:-0}
-RSYNC_MAXDELETE=${RSYNC_MAXDELETE:-4000}
-RSYNC_OPTIONS=${RSYNC_OPTIONS:-'-4pPrltvHSB8192 --partial-dir=.rsync-partial --timeout 14400 --delay-updates --safe-links --delete-delay --delete-excluded'}
+if [[ $PREFER_IPV6 = true ]]; then
+    OPTS+=' --ipv6'
+else
+    OPTS+=' --ipv4'
+fi
+[[ $RSYNC_DELAY_UPDATES = true ]] && OPTS+=' --delay-updates'
+[[ $RSYNC_BLKSIZE -ne 0 ]] && OPTS+=" --block-size ${RSYNC_BLKSIZE}"
+[[ -n $BIND_ADDRESS ]] && OPTS+=" --address $BIND_ADDRESS"
+
 RSYNC_EXCLUDE+=' --exclude .~tmp~/'
 
-exec rsync $RSYNC_EXCLUDE --bwlimit "$RSYNC_BW" --max-delete "$RSYNC_MAXDELETE" --address "$BIND_ADDRESS" $RSYNC_OPTIONS "$RSYNC_HOST::$RSYNC_PATH" "$TO"
+exec rsync $RSYNC_EXCLUDE --bwlimit "$RSYNC_BW" --max-delete "$RSYNC_MAXDELETE" $OPTS $RSYNC_EXTRA_OPTS "$RSYNC_HOST::$RSYNC_PATH" "$TO"
