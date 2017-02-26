d37a29c3ff1cc2206477ed055e9ed7b3a69a7f55
Yifan Gao <git@yfgao.com>
2017-02-26 16:02:39 +0800

    add pypi

---
+++ pypi/pre-sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF > /etc/bandersnatch.conf
+[mirror]
+directory = $TO
+master = $PYPI_MASTER
+timeout = $BANDERSNATCH_TIMEOUT
+workers = $BANDERSNATCH_WORKERS
+hash-index = false
+stop-on-error = $BANDERSNATCH_STOP_ON_ERROR
+delete-packages = true
+EOF
+
+chown $OWNER $TO
