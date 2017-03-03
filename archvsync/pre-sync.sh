7302799b85671c3546927275e4ce99bca9627aec
knight42 <anonymousknight96@gmail.com>
2017-03-03 21:30:50 +0800

    archvsync: pkill rsync if signal INT received

--- archvsync/pre-sync.sh
+++ archvsync/pre-sync.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# override the `killer` func in entry.sh
+killer() {
+    kill -- "$1"
+    pkill rsync
+    wait "$1"
+}
+
 export LOG_ROTATE_CYCLE=0
 export LOG="$LOGFILE"
 mkdir -p "$BASEDIR/etc"
