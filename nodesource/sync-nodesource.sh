4a9e2189abd6d828ea404ea311539ce1613656a7
Yifan Gao <git@yfgao.com>
2017-10-26 16:52:36 +0800

    Beautify sync-nodesource.sh

--- nodesource/sync-nodesource.sh
+++ nodesource/sync-nodesource.sh
@@ -6,8 +6,7 @@ export LFTPSYNC_PATH=
 export LFTPSYNC_EXCLUDE="-X db/ -X conf/"
 
 sync_nodesource(){
-    local DIST=$1
-    shift
+    local DIST=$1; shift
     while [[ $# -gt 0 ]]; do
         local VERSION=$1; shift
         local DIR=$BASE/$DIST/$VERSION
@@ -17,6 +16,5 @@ sync_nodesource(){
     done
 }
 
-sync_nodesource deb {node_{4..8}.x,node_0.1{0,2},iojs_{1..3}.x}
-sync_nodesource rpm {pub_{4..8}.x,pub_0.1{0,2}}
-
+sync_nodesource deb node_{4..8}.x node_0.1{0,2} iojs_{1..3}.x
+sync_nodesource rpm pub_{4..8}.x pub_0.1{0,2}
