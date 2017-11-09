1cf5320f63c93ca2b5a834a80fc0c3e8e57a1800
knight42 <anonymousknight96@gmail.com>
2017-11-09 15:12:28 +0800

    chore: set default TRAVIS_COMMIT_RANGE

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 export ORG=ustcmirror
 
 is_modified() {
-    [[ -z $TRAVIS_COMMIT_RANGE ]] && return 1 # false
+    TRAVIS_COMMIT_RANGE=${TRAVIS_COMMIT_RANGE:-'origin/master...HEAD'}
     local COMMIT_FROM=${TRAVIS_COMMIT_RANGE%...*}
     local COMMIT_TO=${TRAVIS_COMMIT_RANGE#*...}
     [[ -n $(git diff "$COMMIT_FROM" "$COMMIT_TO" -- "$1") ]]
