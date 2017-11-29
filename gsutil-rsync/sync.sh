80f339f4c1bf274597e519de1dc484b478d11649
knight42 <anonymousknight96@gmail.com>
2017-11-29 14:33:33 +0800

    feat(gsutil): exclude files

--- gsutil-rsync/sync.sh
+++ gsutil-rsync/sync.sh
@@ -8,8 +8,16 @@
 ## SET IN ENVIRONMENT VARIABLES
 #DEBUG=
 #GS_URL=
+#GS_EXTRA=
+#GS_EXCLUDE=
+
+GS_EXTRA="${GS_EXTRA:-}"
+GS_EXCLUDE="${GS_EXCLUDE:-}"
 
 set -eu
-[[ $DEBUG = true ]] && set -x
+if [[ $DEBUG = true ]]; then
+    GS_EXTRA+=" -D"
+    set -x
+fi
 
-exec gsutil -m rsync -d -r "$GS_URL" "$TO"
+exec gsutil -m $GS_EXTRA rsync -C -d -r -U $GS_EXCLUDE "$GS_URL" "$TO"
