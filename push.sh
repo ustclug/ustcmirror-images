f3a75230014449e76726ec54eeb2cb0af634784b
knight42 <anonymousknight96@gmail.com>
2018-03-13 15:54:55 +0800

    chore: correct the filter

--- push.sh
+++ push.sh
@@ -2,16 +2,16 @@
 
 [[ $DEBUG = true ]] && set -x
 
-if [[ $TRAVIS_EVENT_TYPE != 'push' && $TRAVIS_EVENT_TYPE != 'cron' ]]; then
+if [[ $TRAVIS_EVENT_TYPE != push && $TRAVIS_EVENT_TYPE != cron ]]; then
     exit 0
 fi
-if [[ $TRAVIS_EVENT_TYPE == 'push' && $TRAVIS_BRANCH != 'master' ]]; then
+if [[ $TRAVIS_EVENT_TYPE == push && $TRAVIS_BRANCH != master ]]; then
     exit 0
 fi
 
 [[ -z $SKIP_LOGIN ]] && docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-for image in $(docker images --filter=label=ustcmirror.images --format="{{.Repository}}:{{.Tag}}")
+for image in $(docker images --filter=label=org.ustcmirror.images=true --format="{{.Repository}}:{{.Tag}}")
 do
     docker push "$image"
     echo "$image pushed"
