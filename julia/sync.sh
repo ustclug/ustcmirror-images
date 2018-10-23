64e71dd50a7a83b10a7dbe9134aaae949ceae4e6
Elliot Saba <staticfloat@gmail.com>
2018-10-23 21:59:56 +0800

    Use environment variables

--- julia/sync.sh
+++ julia/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -p $TO/tmp
-python3 /julia-mirror/scripts/mirror_julia.py /data --logging-file /logs/mirror_julia.log --temp-dir=/data/tmp
-rm -rf /data/tmp
+mkdir -p ${TO}/tmp
+python3 /julia-mirror/scripts/mirror_julia.py ${TO} --logging-file ${LOGDIR}/mirror_julia.log --temp-dir=${TO}/tmp
+rm -rf ${TO}/tmp
