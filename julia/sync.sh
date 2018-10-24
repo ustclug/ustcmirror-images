fcc4a871b3164779c6fb8faa3e2db32286798c28
Elliot Saba <staticfloat@gmail.com>
2018-10-24 16:29:59 +0800

    don't log to file, just log to stdout

--- julia/sync.sh
+++ julia/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p ${TO}/tmp
-python3 /julia-mirror/scripts/mirror_julia.py ${TO} --logging-file ${LOGDIR}/mirror_julia.log --temp-dir=${TO}/tmp
+python3 /julia-mirror/scripts/mirror_julia.py ${TO} --temp-dir=${TO}/tmp
 rm -rf ${TO}/tmp
