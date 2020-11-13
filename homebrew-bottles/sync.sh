203bd7cf0bfe6fd5ba8183435f0fee3b631fc87b
taoky <taoky@mail.ustc.edu.cn>
2020-11-13 13:39:09 +0000

    Add HOMEBREW_REPO parameter to homebrew-bottles

--- homebrew-bottles/sync.sh
+++ homebrew-bottles/sync.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 echo "> update package info..."
+export HOMEBREW_REPO=${HOMEBREW_REPO:-"git://github.com/homebrew/homebrew-core.git"}
 export HOME=/root
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 cd ~/.linuxbrew/Library/Taps/homebrew/homebrew-core || exit 1
 git init
-git remote add origin git://github.com/homebrew/homebrew-core.git
+git remote add origin "$HOMEBREW_REPO"
 git fetch --depth=1 origin master
 git reset --hard origin/master
 export HOMEBREW_CACHE=$TO/bottles
