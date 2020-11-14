62a396a704e27e53e277e571d36ff6c70ce16a14
taoky <taoky@mail.ustc.edu.cn>
2020-11-14 19:19:58 +0800

    statically link stackage.hs
    It has warnings, but it is running on the same env during build & run

--- stackage/prepare.sh
+++ stackage/prepare.sh
@@ -6,5 +6,5 @@ mkdir /root/.cabal
 mv config /root/.cabal/
 cabal update
 cabal install yaml process containers aeson --force-reinstalls
-ghc -O2 stackage.hs
+ghc -O2 -optl-static stackage.hs
 rm -rf /root/.cabal && apt-get purge -y --auto-remove libghc-bzlib-dev xz-utils musl-dev apt-utils ghc cabal-install haskell-platform libghc-cabal-dev && rm -rf /var/lib/apt/lists/*
