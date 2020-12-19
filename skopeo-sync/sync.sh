51e0c2e1d476701752c0ac38ac35c05b9f333ce6
taoky <taoky99@outlook.com>
2020-12-19 23:04:05 +0800

    skopeo: set sync dest to registry

--- skopeo-sync/sync.sh
+++ skopeo-sync/sync.sh
@@ -1,32 +1,4 @@
 #!/usr/bin/env bash
 
-cat > /etc/skopeo-images.yaml <<EOF
-k8s.gcr.io:
-  images:
-    pause:
-      - 3.1
-      - 3.2
-    kube-apiserver:
-      - v1.18.3
-      - v1.18.4
-      - v1.18.5
-    kube-controller-manager:
-      - v1.18.3
-      - v1.18.4
-      - v1.18.5
-    kube-scheduler:
-      - v1.18.3
-      - v1.18.4
-      - v1.18.5
-    kube-proxy:
-      - v1.18.3
-      - v1.18.4
-      - v1.18.5
-    etcd:
-      - 3.4.3-0
-      - 3.4.7-0
-    coredns:
-      - 1.6.7
-EOF
-
-exec skopeo --insecure-policy sync --src yaml --dest docker /etc/skopeo-images.yaml "$TO"
+skopeo login -u mirror -p "$REGISTRY_PASSWD" "$REGISTRY_HOST"
+exec skopeo --insecure-policy sync --scoped --src yaml --dest docker /etc/skopeo-images.yaml "$REGISTRY_HOST"
