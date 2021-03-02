c31319bb0dbaa9edc11723de4424aca74607e2e8
Yifan Gao <git@yfgao.com>
2021-03-02 20:11:08 +0800

    homebrew-bottles: code annotation

--- homebrew-bottles/sync.sh
+++ homebrew-bottles/sync.sh
@@ -23,7 +23,16 @@ if [[ $? -ne 0 ]]; then
     echo "[FATAL] download meta-data failed."
     exit 3
 fi
+
+# extract sha256 and URL from JSON result
 jq -r ' .[].bottle | .[].files | .[] | "\(.sha256) \(.url)"' < $FORMULA_JSON > $BOTTLES
+
+# get common prefix of URLs
+# at the time of 2021-3-2 the value set is:
+# https://homebrew.bintray.com
+# https://dl.bintray.com/homebrew
+# https://linuxbrew.bintray.com
+# https://dl.bintray.com/linuxbrew
 URL_BASE=$(grep -oP 'https?://.+(?=/bottles/[^/]+.gz$)' $BOTTLES | uniq)
 if [[ -z $URL_BASE ]]; then
     echo "[FATAL] unexpected URL format, please report."
@@ -31,17 +40,21 @@ if [[ -z $URL_BASE ]]; then
 elif [[ `wc -l <<<$URL_BASE` != 1 ]]; then
     echo "[WARN] inconsistent URL base."
 fi
+
+# if not specified, use the first URL prefix extracted from JSON API
+# this could take a risk, while upstream use different bottle sites for diffrent formulea
 HOMEBREW_BOTTLE_DOMAIN=${HOMEBREW_BOTTLE_DOMAIN:-$(head -n 1 <<<$URL_BASE)}
 
 # drop URL common base, leave only "bottles/*.tar.gz"
 sed -i 's|https\?://.\+/\(bottles/.\+\.gz\)$|\1|' $BOTTLES
 
 # JSON API mixing linuxbrew bottles and homebrew bottles
-# we need to filtering linuxbrew one
+# we need to filtering linux one
 if [[ $TARGET_OS == linux ]]; then
 	sed -i '/x86_64_linux/!d' $BOTTLES
 fi
 
+# parallel downloading
 export by_hash=$(realpath $TO/.by-hash)
 export by_hash_pattern="./.by-hash/*"
 export remote_url=$HOMEBREW_BOTTLE_DOMAIN
@@ -51,9 +64,13 @@ enable_checksum=true parallel --line-buffer -j $HOMEBREW_BOTTLES_JOBS --pipepart
 # urldecode
 gawk -i inplace  -niord '{printf RT?$0chr("0x"substr(RT,2)):$0}' RS=%.. $BOTTLES
 
+# clean up outdated bottles
 removal_list=$(mktemp)
 cd $local_dir
 comm -23 <(find . -type f -not -path "$by_hash_pattern" | sed "s|^./||" | sort) <(awk '{print $2}' $BOTTLES | sort) | tee $removal_list | xargs rm -f
 sed 's/^/[INFO] remove /g' $removal_list
+
+# clean empty dir. If enerything work as expect, this command would do nothing
+find . -type d -empty -delete
 
 clean_hash_file
