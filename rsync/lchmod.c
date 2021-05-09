10c2625e639999668aa24e23b4c2b3918d63bb45
Yifan Gao <git@yfgao.com>
2021-05-09 18:21:57 +0800

    rsync: lchmod patch check if a symlink

--- rsync/lchmod.c
+++ rsync/lchmod.c
@@ -1,6 +1,17 @@
 #include <sys/stat.h>
+#include <fcntl.h>
 
-int lchmod(const char *path, mode_t mode)
-{
-    return 0;
+int lchmod(const char *path, mode_t mode) {
+    struct stat st;
+
+    int r = lstat(path, &st);
+    if (r < 0) {
+        return r;
+    }
+
+    if (S_ISLNK(st.st_mode)) {
+        return 0;
+    } else {
+        return fchmodat(AT_FDCWD, path, mode, AT_SYMLINK_NOFOLLOW);
+    }
 }
