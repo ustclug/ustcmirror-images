ee7cbf85223119e71b97763f14cf416041ed85cd
iBug <git@ibugone.com>
2021-06-16 13:54:27 +0800

    Replace shell script with Python (#74)

--- apt-sync/sync.sh
+++ apt-sync/sync.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
 
-set -e
-[[ $DEBUG = true ]] && set -x
-
-if [[ -n "$APTSYNC_UNLINK" ]]; then
-  DELETE=--delete
-fi
-
 cd /usr/local/lib/tunasync
-DISTS="$APTSYNC_DISTS:"
-RET=0
-while [[ "$DISTS" == *:* ]]; do
-  THISDIST="${DISTS%%:*}"
-  THISDIST="${THISDIST%"${THISDIST##*[![:space:]]}"}"
-  THISDIST="${THISDIST#"${THISDIST%%[![:space:]]*}"}"
-  THISDIST="${THISDIST}|"
-  DISTS="${DISTS#*:}"
-
-  APT_DIST="${THISDIST}"
-  APT_COMP="${APT_DIST#*|}"
-  APT_ARCH="${APT_COMP#*|}"
-  APT_DIR="${APT_ARCH#*|}"
-
-  APT_DIST="${THISDIST%%|*}"
-  APT_COMP="${APT_COMP%%|*}"
-  APT_ARCH="${APT_ARCH%%|*}"
-  APT_DIR="${APT_DIR%%|*}"
-
-  APT_ARCH="${APT_ARCH// /,}"
-  APT_COMP="${APT_COMP// /,}"
-
-  python3 apt-sync.py $DELETE "$APTSYNC_URL""${APT_DIR}" "$APT_DIST" "$APT_COMP" "$APT_ARCH" "${TO}/${APT_DIR}" || RET=$((RET+$?))
-done
-
-exit $RET
+exec python3 /sync.py
