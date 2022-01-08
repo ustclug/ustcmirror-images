bd642e2e212c82fb14d6fffd96d7796bc219786d
ksqsf <i@ksqsf.moe>
2022-01-08 19:14:33 +0800

    ghcup: fix prepare.sh

--- ghcup/prepare.sh
+++ ghcup/prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -e
+set -e
 
 mkdir /root/.cabal
 mv config /root/.cabal/
@@ -10,11 +10,10 @@ apt-get install -y aria2 ca-certificates git wget xz-utils apt-utils build-essen
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_MINIMAL=1 sh
 source /root/.ghcup/env
 ghcup install ghc 9.0.2
-cat /root/.ghcup/logs/*
 ghcup install cabal 3.6.2.0
 ghcup set ghc 9.0.2
 cabal update
 cabal build -O2
-cp $(cabal list-bin ghcupsync) /
+cp $(cabal list-bin -O2 ghcupsync) /
 
 rm -rf /dist* /root/.cabal /root/.ghcup /root/.ghc && apt-get purge -y --auto-remove build-essential curl libffi-dev libgmp-dev libncurses-dev libnuma-dev && rm -rf /var/lib/apt/lists/*
