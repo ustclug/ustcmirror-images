b8640df77694120e7da574484593abd48deb5515
taoky <taoky1999@gmail.com>
2022-03-18 15:44:48 +0800

    crates-io-index: add

---
+++ crates-io-index/sync.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Migrated to yuki (docker) by @taoky
+# Created by @ksqsf
+# Based on the original version written by @knight42
+
+# ENVS:
+# CRATES_PROXY=
+# CRATES_GITMSG=
+
+set -e
+[[ -n $DEBUG ]] && set -x
+
+CRATES_PROXY="${CRATES_PROXY:-https://crates-io.proxy.ustclug.org/api/v1/crates}"
+CRATES_GITMSG="${CRATES_GITMSG:-Redirect to USTC Mirrors}"
+
+ensure_redirect() {
+    pushd "$TO"
+    if grep -F -q "$CRATES_PROXY" 'config.json'; then
+        return
+    else
+        cat <<EOF > 'config.json'
+{
+    "dl": "$CRATES_PROXY",
+    "api": "https://crates.io/"
+}
+EOF
+        git add config.json
+        git commit -qm "$CRATES_GITMSG"
+    fi
+    popd
+}
+
+# crates.io-index has a custom ensure_redirect logic
+# so now we don't use gitsync here.
+
+if [[ -d $TO ]]; then
+    cd "$TO"
+    git fetch origin
+    git reset --hard origin/master
+    ensure_redirect
+    git repack -adb
+    git gc --auto
+    git update-server-info
+else
+    git clone 'https://github.com/rust-lang/crates.io-index.git' "$TO"
+    ensure_redirect
+fi
