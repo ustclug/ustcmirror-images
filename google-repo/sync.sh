f18aa79bbf29a5f8a18faa62ecf57342300dc930
taoky <me@taoky.moe>
2022-12-07 06:34:10 +0800

    google-repo: always output tracing to stderr, and cleanup TRACE_FILE

--- google-repo/sync.sh
+++ google-repo/sync.sh
@@ -18,9 +18,15 @@ fi
 # Now start syncing
 echo "start repo sync"
 repo_ret=0
-# Don't let syncing failure stop repacking
-repo --trace sync || repo_ret=$?
+
+export PYTHONUNBUFFERED=1
+# https://gerrit.googlesource.com/git-repo/+/a6c52f566acfbff5b0f37158c0d33adf05d250e5 (2022/11/03) changes behavior of tracing
+# from directly outputting to storing to .repo/TRACE_FILE
+repo --trace --trace-to-stderr sync || repo_ret=$?
+rm -f "$TO"/.repo/TRACE_FILE
+
 if [[ ! $repo_ret == 0 ]]; then
+    # Don't let syncing failure stop repacking
     echo "[warn]: repo sync returns an unzero exit code: $repo_ret"
 fi
 echo "start repacking git objects"
