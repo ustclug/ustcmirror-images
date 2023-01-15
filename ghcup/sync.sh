818971cd35093c527c75a45a987b1a2747d02819
ksqsf <i@ksqsf.moe>
2023-01-15 13:50:39 +0800

    ghcup: use git version of bootstrap scripts (#86)
    Revert "ghcup: always keep ghcup files on GC"

--- ghcup/sync.sh
+++ ghcup/sync.sh
@@ -7,9 +7,9 @@ set -o pipefail
 
 mkdir -p "$TO/sh"
 pushd "$TO/sh"
-curl https://get-ghcup.haskell.org -o bootstrap-haskell
+curl https://raw.githubusercontent.com/haskell/ghcup-hs/master/scripts/bootstrap/bootstrap-haskell -o bootstrap-haskell
 sed -i 's|https://downloads.haskell.org/~ghcup|https://mirrors.ustc.edu.cn/ghcup/downloads.haskell.org/~ghcup|' bootstrap-haskell
-curl https://www.haskell.org/ghcup/sh/bootstrap-haskell.ps1 -o bootstrap-haskell.ps1
+curl https://raw.githubusercontent.com/haskell/ghcup-hs/master/scripts/bootstrap/bootstrap-haskell.ps1 -o bootstrap-haskell.ps1
 sed -i 's|https://www.haskell.org/ghcup/sh/bootstrap-haskell|https://mirrors.ustc.edu.cn/ghcup/sh/bootstrap-haskell|' bootstrap-haskell.ps1
 sed -i 's|https://repo.msys2.org/distrib/|https://mirrors.ustc.edu.cn/msys2/distrib/|' bootstrap-haskell.ps1
 popd
