962b33e8d7c159d8f7bfdcce5064ae28a6518a9d
taoky <me@taoky.moe>
2023-02-08 19:29:12 +0800

    homebrew-bottles: add API handling

--- homebrew-bottles/bottles-json/src/main.rs
+++ homebrew-bottles/bottles-json/src/main.rs
@@ -1,6 +1,10 @@
 // #![feature(try_trait)]
 
+use std::{collections::HashSet, fs::File, path::PathBuf};
+
+use clap::{Parser, ValueEnum};
 use serde::Deserialize;
+use serde_json::Value;
 
 #[derive(Deserialize)]
 struct Versions {
@@ -36,6 +40,33 @@ struct BottleInfo {
     sha256: String,
 }
 
+#[derive(ValueEnum, Clone)]
+enum Mode {
+    GetBottlesMetadata,
+    ExtractJson,
+}
+
+#[derive(ValueEnum, Clone)]
+enum Type {
+    Forumla,
+    Cask,
+}
+
+#[derive(Parser)]
+struct Cli {
+    /// Choose from get-bottles-metadata (default) or extract-json
+    #[arg(long, value_enum, default_value_t=Mode::GetBottlesMetadata)]
+    mode: Mode,
+
+    /// The folder to extract json to, required when mode is extract-json
+    #[arg(long)]
+    folder: Option<PathBuf>,
+
+    /// Choose from formula (default) or cask
+    #[arg(long, value_enum, default_value_t=Type::Forumla)]
+    type_: Type,
+}
+
 fn d(f: &Formula) -> Option<()> {
     if f.versions.bottle {
         let bs = f.bottle.stable.as_ref()?;
@@ -47,9 +78,17 @@ fn d(f: &Formula) -> Option<()> {
                     url = bi.url,
                     name = f.name,
                     version = f.versions.stable.as_ref()?,
-                    revision = if f.revision == 0 { "".to_owned() } else { format!("_{}", f.revision) },
+                    revision = if f.revision == 0 {
+                        "".to_owned()
+                    } else {
+                        format!("_{}", f.revision)
+                    },
                     platform = platform,
-                    rebuild = if bs.rebuild == 0 { "".to_owned() } else { format!(".{}", bs.rebuild) },
+                    rebuild = if bs.rebuild == 0 {
+                        "".to_owned()
+                    } else {
+                        format!(".{}", bs.rebuild)
+                    },
                 );
             }
         }
@@ -57,9 +96,64 @@ fn d(f: &Formula) -> Option<()> {
     Some(())
 }
 
+fn e(f: &Value, name: &str, target: &PathBuf) -> Option<()> {
+    let tmp_name = format!("{}.json.new", name);
+    let final_name = format!("{}.json", name);
+
+    {
+        let file = File::create(target.join(tmp_name.clone())).unwrap();
+        serde_json::to_writer(file, f).unwrap();
+    }
+    std::fs::rename(target.join(tmp_name), target.join(final_name)).unwrap();
+    Some(())
+}
+
 fn main() {
-    let f: Formulae = serde_json::from_reader(std::io::stdin()).unwrap();
-    for f in f.0 {
-        d(&f);
+    let cli = Cli::parse();
+
+    match cli.mode {
+        Mode::GetBottlesMetadata => {
+            let f: Formulae = serde_json::from_reader(std::io::stdin()).unwrap();
+            for f in f.0 {
+                if d(&f).is_none() {
+                    eprintln!("Failed to parse formula: {}", f.name);
+                }
+            }
+        }
+        Mode::ExtractJson => {
+            // Handle API json weak-typed here, as it may cause too much trouble to write all types of all fields
+            let f: Value = serde_json::from_reader(std::io::stdin()).unwrap();
+            let target_dir = cli
+                .folder
+                .expect("target folder is required as an argument");
+            let mut existing_jsons = std::fs::read_dir(&target_dir)
+                .unwrap()
+                .filter_map(|e| {
+                    Some(
+                        e.unwrap()
+                            .file_name()
+                            .into_string()
+                            .unwrap()
+                            .strip_suffix(".json")?
+                            .to_owned(),
+                    )
+                })
+                .collect::<HashSet<_>>();
+            for f in f.as_array().unwrap() {
+                let fname = match cli.type_ {
+                    Type::Forumla => &f["name"],
+                    Type::Cask => &f["token"],
+                };
+                let fname = fname.as_str().unwrap();
+                if e(f, fname, &target_dir).is_none() {
+                    eprintln!("Failed to extract json: {}", fname);
+                }
+                existing_jsons.remove(fname);
+            }
+            // Clean unused jsons
+            for fname in existing_jsons {
+                std::fs::remove_file(target_dir.join(format!("{}.json", fname))).unwrap();
+            }
+        }
     }
 }
--- homebrew-bottles/sync.sh
+++ homebrew-bottles/sync.sh
@@ -4,18 +4,43 @@ HOMEBREW_BOTTLES_JOBS=${HOMEBREW_BOTTLES_JOBS:-1}
 
 source /curl-helper.sh
 
-FORMULA_JSON=$(mktemp)
+FORMULA_JSON="$TO/api/formula.json"
+CASK_JSON="$TO/api/cask.json"
+mkdir -p "$TO/api"
+mkdir -p "$TO/api/formula"
+mkdir -p "$TO/api/cask"
+
 BOTTLES=$(mktemp)
-URL_JSON=https://formulae.brew.sh/api/formula.json
+FORMULA_URL_JSON=https://formulae.brew.sh/api/formula.json
+CASK_URL_JSON=https://formulae.brew.sh/api/cask.json
 
 curl_init
 
-$CURL_WRAP -sSL -o "$FORMULA_JSON" "$URL_JSON"
+# Step 1: Download new API jsons and extract
+$CURL_WRAP -sSL -o "$FORMULA_JSON".tmp "$FORMULA_URL_JSON"
 if [[ $? -ne 0 ]]; then
-	echo "[FATAL] download meta-data failed."
+	echo "[FATAL] download formula meta-data failed."
+	exit 2
+fi
+$CURL_WRAP -sSL -o "$CASK_JSON".tmp "$CASK_URL_JSON"
+if [[ $? -ne 0 ]]; then
+	echo "[FATAL] download cask meta-data failed."
 	exit 3
 fi
+bottles-json --mode extract-json --type formula --folder "$TO/api/formula" < "$FORMULA_JSON".tmp
+if [[ $? -ne 0 ]]; then
+    echo "[FATAL] formula API json extracting failed."
+    exit 5
+fi
+bottles-json --mode extract-json --type cask --folder "$TO/api/cask" < "$CASK_JSON".tmp
+if [[ $? -ne 0 ]]; then
+	echo "[FATAL] cask API json extracting failed."
+	exit 6
+fi
+mv "$FORMULA_JSON".tmp "$FORMULA_JSON"
+mv "$CASK_JSON".tmp "$CASK_JSON"
 
+# Step 2: Download bottles (formula only)
 # extract sha256, URL and file name from JSON result
 bottles-json < $FORMULA_JSON > $BOTTLES
 if [[ $? -ne 0 ]]; then
