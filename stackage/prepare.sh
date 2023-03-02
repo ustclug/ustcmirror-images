237f2a30d9de8d1d7fcfcd096ad57d2b5d080fa0
taoky <me@taoky.moe>
2023-03-02 16:06:21 +0800

    stackage: pin yaml and aeson version
    Fix build failure: https://github.com/ustclug/ustcmirror-images/actions/runs/4308972921/jobs/7515831531

--- stackage/prepare.sh
+++ stackage/prepare.sh
@@ -5,6 +5,6 @@ apt-get install -y aria2 ca-certificates git wget libghc-bzlib-dev xz-utils musl
 mkdir /root/.cabal
 mv config /root/.cabal/
 cabal update
-cabal install --lib yaml process containers aeson
+cabal install --lib yaml-0.11.8.0 process containers aeson-2.1.1.0
 ghc -O2 stackage.hs -package unordered-containers -package split
 rm -rf /root/.cabal && apt-get purge -y --auto-remove libghc-bzlib-dev xz-utils musl-dev apt-utils ghc cabal-install haskell-platform libghc-cabal-dev && rm -rf /var/lib/apt/lists/*
