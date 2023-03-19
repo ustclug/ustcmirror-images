7eab38bceeaa5a6842c626c1674fcc866f869216
RTXUX <rtxux@ustclug.org>
2023-03-19 04:10:53 +0000

    debian-cd: verify jigdo-bin-0.7.3.tar.bz2

--- debian-cd/prepare.sh
+++ debian-cd/prepare.sh
@@ -3,6 +3,7 @@ set -e
 apk add --no-cache wget ca-certificates rsync
 mkdir -p /etc/jigdo
 wget 'https://ftp.lug.ustc.edu.cn/misc/jigdo-bin-0.7.3.tar.bz2'
+echo "58b8a6885822e55f365c99131c906f16ceaaf657c566e10f410d026704cad157  jigdo-bin-0.7.3.tar.bz2" | sha256sum -c -
 tar xf jigdo-bin-0.7.3.tar.bz2 jigdo-bin-0.7.3/jigdo-file
 mv jigdo-bin-0.7.3/jigdo-file /usr/local/bin/
 rm -rf jigdo-bin-*
