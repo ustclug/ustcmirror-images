60aabce3f69fd76a470d415e1ab1f827424cf542
taoky <me@taoky.moe>
2023-12-30 22:40:52 +0800

    Use stack for stackage image dep management
    But it's too slow to build and not tested.
    See https://github.com/ustclug/ustcmirror-images/issues/105 for more information.

--- stackage/prepare.sh
+++ stackage/prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 apt-get update
-apt-get install -y aria2 ca-certificates git wget libghc-bzlib-dev xz-utils musl-dev apt-utils ghc cabal-install haskell-platform libghc-cabal-dev
+apt-get install -y aria2 ca-certificates git haskell-stack g++
+stack upgrade --binary-only
 mkdir /root/.cabal
 mv config /root/.cabal/
-cabal update
-cabal install --lib yaml-0.11.8.0 process containers aeson-2.1.1.0
-ghc -O2 stackage.hs -package unordered-containers -package split
-rm -rf /root/.cabal && apt-get purge -y --auto-remove libghc-bzlib-dev xz-utils musl-dev apt-utils ghc cabal-install haskell-platform libghc-cabal-dev && rm -rf /var/lib/apt/lists/*
+stack build
+cp $(stack path --local-install-root)/bin/stackage /
+rm -rf /root/.cabal && apt-get purge -y --auto-remove haskell-stack g++ && rm -rf /var/lib/apt/lists/*
