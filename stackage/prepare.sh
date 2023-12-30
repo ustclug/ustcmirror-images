7223e9f11bd6a738894656bc18a93dda400b4805
taoky <me@taoky.moe>
2023-12-30 23:28:23 +0800

    stackage: fix build error in shell script

--- stackage/prepare.sh
+++ stackage/prepare.sh
@@ -5,6 +5,6 @@ HASKELL_DEPS="ghc libghc-yaml-dev libghc-process-extras-dev \
               libghc-aeson-dev libghc-split-dev"
 
 apt-get update
-apt-get install -y aria2 ca-certificates git "$HASKELL_DEPS"
+apt-get install -y aria2 ca-certificates git $HASKELL_DEPS
 ghc -O2 stackage.hs
-apt-get purge --auto-remove -y "$HASKELL_DEPS"
+apt-get purge --auto-remove -y $HASKELL_DEPS
