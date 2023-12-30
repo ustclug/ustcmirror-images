8b1e9294599927780985c011c09fb903fba68ed2
taoky <me@taoky.moe>
2023-12-31 00:01:49 +0800

    Use Debian Haskell packages for ghcup
    cabal file is still kept for development and debugging.

--- ghcup/prepare.sh
+++ ghcup/prepare.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-mkdir /root/.cabal
-mv config /root/.cabal/
+HASKELL_DEPS="ghc libghc-aeson-dev libghc-lens-dev libghc-lens-aeson-dev \
+              libghc-network-uri-dev libghc-split-dev libghc-typed-process-dev \
+              libghc-yaml-dev"
+# Please use ldd to check the dependencies of compiled binary
+HASKELL_KEEP="libyaml-0-2"
 
 apt-get update
-apt-get install -y aria2 ca-certificates git wget xz-utils apt-utils build-essential curl libffi-dev libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 libnuma-dev
+apt-get install -y aria2 ca-certificates git $HASKELL_DEPS $HASKELL_KEEP
 
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_MINIMAL=1 sh
-source /root/.ghcup/env
-ghcup install ghc 9.0.2
-ghcup install cabal 3.6.2.0
-ghcup set ghc 9.0.2
-cabal update
-cabal build -O2
-cp $(cabal list-bin -O2 ghcupsync) /
+# compile with Haskell language extensions
+ghc -O2 -XScopedTypeVariables -XOverloadedStrings ghcupsync.hs
 
-rm -rf /dist* /root/.cabal /root/.ghcup /root/.ghc && apt-get purge -y --auto-remove build-essential libffi-dev libgmp-dev libncurses-dev && rm -rf /var/lib/apt/lists/*
+rm -rf /ghcupsync.* && apt-get purge -y --auto-remove $HASKELL_DEPS && rm -rf /var/lib/apt/lists/*
