9d08c2f12ec99aade069a4a1b9ab24450af27a81
Erchiusx <99174225+Erchiusx@users.noreply.github.com>
2025-04-23 21:37:22 +0800

    Erchiusx ghcup bootstrap cabal (#133)
    * ghcup: add a `latest` symlink for each channel
    
    addressing https://github.com/ustclug/mirrorhelp/pull/271
    
    * ghcup: add signature for each latest channel
    
    * ghcup: bootstrap with cabal config modified 
    
    During the installation of cabal,
    GHCup would update cabal as soon as it's installed;
    But at that point, Cabal config is not adjusted to ustcmirror.
    We add a logic to addjust cabal config before cabal updating.

--- ghcup/sync.sh
+++ ghcup/sync.sh
@@ -9,6 +9,8 @@ mkdir -p "$TO/sh"
 pushd "$TO/sh"
 curl https://raw.githubusercontent.com/haskell/ghcup-hs/master/scripts/bootstrap/bootstrap-haskell -o bootstrap-haskell
 sed -i 's|https://downloads.haskell.org/~ghcup|https://mirrors.ustc.edu.cn/ghcup/downloads.haskell.org/~ghcup|' bootstrap-haskell
+sed -i '/edo cabal update/ i \ \ \ \ edo cabal user-config update -a "repository mirrors.ustc.edu.cn" -a "  url: https://mirrors.ustc.edu.cn/hackage/" ' bootstrap-haskell
+# make ghcup init cabal config before cabal update
 curl https://raw.githubusercontent.com/haskell/ghcup-hs/master/scripts/bootstrap/bootstrap-haskell.ps1 -o bootstrap-haskell.ps1
 sed -i 's|https://www.haskell.org/ghcup/sh/bootstrap-haskell|https://mirrors.ustc.edu.cn/ghcup/sh/bootstrap-haskell|' bootstrap-haskell.ps1
 sed -i 's|https://repo.msys2.org/distrib/|https://mirrors.ustc.edu.cn/msys2/distrib/|' bootstrap-haskell.ps1
