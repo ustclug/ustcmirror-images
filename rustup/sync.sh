33ef108f33d32f2c542644db25c94ad72984285b
Erina Init <erina@mail.ustc.edu.cn>
2025-08-02 12:27:41 +0800

    updated README.md, updated parameter names

--- rustup/sync.sh
+++ rustup/sync.sh
@@ -5,15 +5,15 @@ set -eu
 
 echo "Running script"
 
-export UPSTREAM_URL="${UPSTREAM_URL:-https://static.rust-lang.org/}"
-export GARBAGE_COLLECT_DAYS="${GARBAGE_COLLECT_DAYS:-1}"
-export MIRROR_TARGETS="${MIRROR_TARGETS:-x86_64-unknown-linux-gnu}"
-export SERVER_URL="${SERVER_URL:-http://127.0.0.1:8000}"
+export UPSTREAM="${UPSTREAM:-https://static.rust-lang.org/}"
+export GC="${GC:-1}"
+export TARGETS="${TARGETS:-x86_64-unknown-linux-gnu}"
+export URL="${URL:-http://127.0.0.1:8000.}"
 
-rustup-mirror --upstream-url "$UPSTREAM_URL" \
-              --targets "$MIRROR_TARGETS" \
-	      --gc "$GARBAGE_COLLECT_DAYS" \
-	      --url "$SERVER_URL" \
+rustup-mirror --upstream-url "$UPSTREAM" \
+              --targets "$TARGETS" \
+	      --gc "$GC" \
+	      --url "$URL" \
 	      --mirror "$TO"
 
 
