b01fd46cbd59893f736bf2d4adfab792d0744578
Erina Init <erina@mail.ustc.edu.cn>
2025-08-03 20:31:54 +0800

    Fixed tabulation for sync.sh

--- rustup/sync.sh
+++ rustup/sync.sh
@@ -11,7 +11,7 @@ export TARGETS="${TARGETS:-x86_64-unknown-linux-gnu}"
 export URL="${URL:-http://127.0.0.1:8000.}"
 
 exec rustup-mirror --upstream-url "$UPSTREAM" \
-              --targets "$TARGETS" \
-	      --gc "$GC" \
-	      --url "$URL" \
-	      --mirror "$TO"
+                   --targets "$TARGETS" \
+                   --gc "$GC" \
+                   --url "$URL" \
+                   --mirror "$TO"
