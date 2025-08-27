2581bf2d48144a178011ef20fd18a83f450ada7b
iBug <git@ibugone.com>
2025-08-27 15:52:12 +0800

    New image: rubygems-dynamic

---
+++ rubygems-dynamic/sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+SPEC_FILES=(latest_specs prerelease_specs specs)
+SPEC_VERSION=4.8
+
+# Fetch specs
+for f in "${SPEC_FILES[@]}"; do
+  fn="$TO/$f.$SPEC_VERSION"
+  wget -O "$fn.gz.new" "$UPSTREAM/$fn"
+  gzip -cd "$fn.gz.new" > "$fn.new"
+  mv -f "$fn.new" "$fn"
+  mv -f "$fn.gz.new" "$fn.gz"
+done
+
+# Reset info API
+find "$TO/info" -type f -delete
+
+# Fetch index
+wget -qO "$TO/versions.new" "$UPSTREAM/versions"
+md5sum "$TO/versions.new" > "$TO/versions.md5sum.new"
+mv -f "$TO/versions.new" "$TO/versions"
+mv -f "$TO/versions.md5sum.new" "$TO/versions.md5sum"
