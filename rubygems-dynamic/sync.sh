d00de80a7afc2ecedee7218bf3b34e0351528bdc
iBug <git@ibugone.com>
2025-08-29 11:33:36 +0800

    Drop gunzipped files

--- rubygems-dynamic/sync.sh
+++ rubygems-dynamic/sync.sh
@@ -10,9 +10,6 @@ for f in "${SPEC_FILES[@]}"; do
   urlpath="/$f.$SPEC_VERSION"
   fn="$TO$urlpath"
   wget -O "$fn.gz.new" "$UPSTREAM$urlpath.gz"
-  gzip -cd "$fn.gz.new" > "$fn.new"
-  touch -r "$fn.gz.new" "$fn.new"
-  mv -f "$fn.new" "$fn"
   mv -f "$fn.gz.new" "$fn.gz"
 done
 
